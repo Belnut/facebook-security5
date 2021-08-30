3eaf9bbf84a3891ab02f3dcaf790e45cbef32494
HyunJin Won <ohj8447@gmail.com>
2021-08-30 21:52:49 +0900

    intra42 login 추가 및 GET /me api 작동 확인

--- src/main/java/sample/api/intra42/Intra42.java
+++ src/main/java/sample/api/intra42/Intra42.java
@@ -5,7 +5,7 @@ import sample.api.ApiBinding;
 import java.util.List;
 
 public class Intra42 extends ApiBinding {
-    private static final String GRAPH_API_BASE_URL = "https://api.intra.42.fr";
+    private static final String GRAPH_API_BASE_URL = "https://api.intra.42.fr/v2";
 
     public Intra42(String accessToken) {
         super(accessToken);
